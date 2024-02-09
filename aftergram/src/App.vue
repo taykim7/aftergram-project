e284f447c305678621e77b8c89e5d53d9df3ae19
taykim7 <th_77@naver.com>
2024-02-10 00:28:17 +0900

    header 추가

--- aftergram/src/App.vue
+++ aftergram/src/App.vue
@@ -1,14 +1,17 @@
 <template>
   <div id="app">
+    <app-header></app-header>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+import AppHeader from './components/common/AppHeader.vue'
 
 export default {
   name: 'App',
   components: {
+    AppHeader,
   }
 }
 </script>
