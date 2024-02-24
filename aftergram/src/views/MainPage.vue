f44663d829730bc22929ffe253c032313471652f
taykim7 <th_77@naver.com>
2024-02-24 13:10:17 +0900

    저장된 데이터 구분, 날짜데이터 형식 변경, 주석 추가

--- aftergram/src/views/MainPage.vue
+++ aftergram/src/views/MainPage.vue
@@ -6,13 +6,15 @@
       {{ selectedYear }}년 {{ selectedMonth }}월
       <!-- <button>오늘</button> -->
       <div>
-        <span v-for="(day, index) in selectDaysArr" :key="index"
-        @click="selectDate(day.split('-')[0], day.split('-')[1], day.split('-')[2])">
+        <span v-for="(day, index) in selectDaysArr"
+        :key="index"
+        :class="0 === day.listGram ? '' : 'saved'"
+        @click="selectDate(day.listDay)">
           <b v-if="3 === index">
-            {{ day.split('-')[2] }}
+            {{ day.listDay.substring(6) }}
           </b>
           <i v-else>
-            {{ day.split('-')[2] }}
+            {{ day.listDay.substring(6) }}
           </i>
         </span>
       </div>
@@ -67,38 +69,43 @@ export default {
     isFuture() {
       return this.today < this.standardDate;
     },
+    isSaved() {
+      return 'saved';
+    },
     posts() {
       return this.$store.state.posts;
     },
   },
   async mounted() {
+    // 저장된 데이터 불러오기
     await this.$store.dispatch('FETCH');
-    console.log(this.$store.state.posts);
     this.today = new Date();
-    this.init();
+    this.initWeek();
   },
   methods: {
-    init() {
+    // 일주일 초기화
+    initWeek() {
       this.selectWeek(this.today.getFullYear(), this.today.getMonth(), this.today.getDate());
       this.initGram();
     },
-
+    // 몸무게 초기화
     initGram() {
       if (this.posts) {
+        // 가장 최근의 데이터를 세팅한다.
         this.gram = this.posts[0].gram;
       } else {
         this.gram =  0;
       }
     },
-
-    // 몸무게 계산
+    // 몸무게 계산 (소수점 제거)
     calgram(gram) {
       this.gram = Math.round((this.gram + gram) * 10) / 10;
     },
-    selectDate(year, month, date) {
-      this.selectWeek(year, month - 1, date);
+    // 날짜 선택
+    selectDate(day) {
+      this.selectWeek(day.substring(0, 4), day.substring(4, 6) - 1, day.substring(6));
+      this.gram = this.savedGram(day);
     },
-
     // 선택된 날 기준 한 주 구성
     selectWeek(year, month, date) {
       this.standardDate = new Date(year, month, date);
@@ -106,17 +113,30 @@ export default {
       this.selectDaysArr = [];
       for (var i = -3; i <= 3; i++) {
         standardAroundDate = new Date(Date.parse(this.standardDate) + i * 1000 * 60 * 60 * 24);
-        this.selectDaysArr.push(`${standardAroundDate.getFullYear()}-${standardAroundDate.getMonth() + 1}-${standardAroundDate.getDate()}`);
+        // this.selectDaysArr.push(this.dateFormat(standardAroundDate));
+        let listDay = this.dateFormat(standardAroundDate);
+        let listGram = this.savedGram(listDay);
+        this.selectDaysArr.push({
+          listDay, listGram
+        });
+        
       }
       // 기준 년, 월
       this.selectedYear = this.standardDate.getFullYear();
       this.selectedMonth = this.standardDate.getMonth() + 1;
       this.selectedDate = this.standardDate.getDate();
     },
+    // 선택한 날짜의 저장된 몸무게
+    savedGram(day) {
+      const savedData = this.$store.state.posts.find(data => data.standardDate === day);
+      return savedData?.gram || 0;
+    },
+    // 메모 열기
     writeMemo() {
       this.memoyn = !this.memoyn;
     },
     // TODO 데이터 양식 수정 (날짜, 무게 등)
+    // 게시글 작성
     async fetchGram() {
       const postData = {
         uid: this.$store.state.uid,
@@ -126,8 +146,9 @@ export default {
         gram: this.gram
       };
       await this.$store.dispatch('POST', postData);
+      alert('저장되었습니다.');
     },
-    // 날짜 포맷팅
+    // 날짜 포맷팅 (YYYYMMDD)
     dateFormat(date) {
       if (date) {
         const year = date.getFullYear();
@@ -141,6 +162,9 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+.saved { 
+  background-color:  rgb(95, 95, 95);
+  color: white;
+  }
 </style>
