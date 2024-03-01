4091ea815d5f0c916caf226626452a2d7d8950bb
taykim7 <th_77@naver.com>
2024-03-01 10:21:14 +0900

    firebaseconfig

---
+++ aftergram/firebaseConfig.js
@@ -0,0 +1,9 @@
+export default {
+  apiKey: "AIzaSyD3T1yNQ90bhH9UrKHiR7ez6hIOWyia7b4",
+  authDomain: "taylab-bc1ab.firebaseapp.com",
+  projectId: "taylab-bc1ab",
+  storageBucket: "taylab-bc1ab.appspot.com",
+  messagingSenderId: "948414322886",
+  appId: "1:948414322886:web:dc24d9d4c3c5cad0b91fc7",
+  measurementId: "G-X38ETBVM40"
+};
